44488e8d348f335351dd808e2fa5db3bc83e278a
Sargam <sargampandey27oct@gmail.com>
2019-12-14 13:26:10 +0530

    added: test case to catch custom exception for correct file path but wrong extension

--- src/test/java/com/iplanaylser/IPLAnalyserTest.java
+++ src/test/java/com/iplanaylser/IPLAnalyserTest.java
@@ -9,6 +9,10 @@ public class IPLAnalyserTest {
 
     private static final String IPL_MOST_RUNS_WRONG_CSV_FILE_PATH = "/home/user/IdeaProjects/IPL-analyser/src/test/resources" +
             "/IPL2019FactsheetMostRuns1.csv";
+
+    private static final String IPL_MOST_RUNS_WRONG_EXT_CSV_FILE_PATH = "/home/user/IdeaProjects/IPL-analyser/src/test/resources" +
+            "/IPL2019FactsheetMostRuns.txt";
+
     @Test
     public void givenIPLMostRunsData_ReturnsExactNoOfPlayersCount() {
         try {
@@ -22,6 +26,15 @@ public class IPLAnalyserTest {
     public void givenWrongIPLMostRunsCSVFile_ShouldThrowIPLAnalyserException() {
         try {
             new IPLAnalyser().loadIPLData(IPL_MOST_RUNS_WRONG_CSV_FILE_PATH);
+        } catch (IPLAnalyserException e) {
+            Assert.assertEquals(IPLAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenCorrectIPLMostRunsCSVFile_ButWrongExtension_ShouldThrowIPLAnalyserException() {
+        try {
+            new IPLAnalyser().loadIPLData(IPL_MOST_RUNS_WRONG_EXT_CSV_FILE_PATH);
         } catch (IPLAnalyserException e) {
             Assert.assertEquals(IPLAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
