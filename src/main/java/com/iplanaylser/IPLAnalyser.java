b4fd2ac7f92ac572d7aae7b9ee646f9f426b330c
Sargam <sargampandey27oct@gmail.com>
2019-12-16 10:15:00 +0530

    refactored: provided main business logic of sorting to analyser class

--- src/main/java/com/iplanaylser/IPLAnalyser.java
+++ src/main/java/com/iplanaylser/IPLAnalyser.java
@@ -7,6 +7,12 @@ import java.util.stream.Collectors;
 public class IPLAnalyser {
 
     Map<String,IPLRuns> iplRunsMap = null;
+    Map<SortingFields, Comparator<IPLRuns>> fields = new HashMap<>();
+
+    public IPLAnalyser() {
+        this.fields.put(SortingFields.AVERAGE, Comparator.comparing(census ->
+                census.avg, Comparator.reverseOrder()));
+    }
 
     public int loadIPLData(String csvFilePath) throws IPLAnalyserException {
         IPLLoader iplLoader = LoadIPLDataProvider.getObject();
@@ -14,10 +20,9 @@ public class IPLAnalyser {
         return iplRunsMap.size();
     }
 
-    public String sortByFields(SortingFields.Parameter parameter) {
-        Comparator<IPLRuns> iplComparator = new SortingFields().getParameter(parameter);
+    public String sortByFields(SortingFields parameter) {
         ArrayList<IPLRuns> list = this.iplRunsMap.values().stream()
-                .sorted(iplComparator)
+                .sorted(this.fields.get(parameter))
                 .collect(Collectors.toCollection(ArrayList::new));
         String sortIplToJson = new Gson().toJson(list);
         return sortIplToJson;
--- src/main/java/com/iplanaylser/SortingFields.java
+++ src/main/java/com/iplanaylser/SortingFields.java
@@ -1,23 +1,7 @@
 package com.iplanaylser;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-
-public class SortingFields {
-    public Map<Parameter, Comparator> sortByFields = new HashMap<>();
-
-    public enum Parameter {
-        AVERAGE
-    }
-
-    public SortingFields() {
-        sortByFields.put(Parameter.AVERAGE, Comparator.<IPLRuns,Double>comparing(iplRuns ->
-                Double.valueOf(iplRuns.avg),Comparator.reverseOrder()));
-    }
-
-    public Comparator getParameter(Parameter fields) {
-        Comparator<IPLRuns> comparator = sortByFields.get(fields);
-        return comparator;
-    }
+public enum  SortingFields {
+    AVERAGE
 }
+
+
