c44c4b64dadd3fb3781974a87eee5ec65c22774f
sargam2797 <sargampandey27oct@gmail.com>
2019-12-23 16:24:20 +0530

    refactored: seperated sorting functionality from analyser to different class to achieve SRP

--- src/main/java/com/iplanaylser/IPLAnalyser.java
+++ src/main/java/com/iplanaylser/IPLAnalyser.java
@@ -5,9 +5,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class IPLAnalyser {
-    private Map<String,IPLDAO> iplRunsMap;
-    private Map<SortingFields, Comparator<IPLDAO>> sortByFields ;
-    private Innings innings;
+     Map<String,IPLDAO> iplMap;
+     private Innings innings;
+     SortingForIPL sortingForIPL;
 
     public enum Innings {
         BATTING,BOWLING
@@ -15,54 +15,20 @@ public class IPLAnalyser {
 
     public IPLAnalyser(Innings innings) {
         this.innings = innings;
-        this.sortByFields = new HashMap<>();
-        this.iplRunsMap = new HashMap<>();
-        setSortByFields();
+        sortingForIPL = new SortingForIPL();
     }
     public IPLAnalyser() {}
 
-    public void setSortByFields() {
-        this.sortByFields.put(SortingFields.BATTING_AVERAGE,Comparator.comparing(census -> census.averageOfBatsmen,
-                Comparator.reverseOrder()));
-        this.sortByFields.put(SortingFields.STRIKING_RATE, Comparator.comparing(census ->
-                census.strikeRate, Comparator.reverseOrder()));
-        this.sortByFields.put(SortingFields.MAX_4s_AND_6s, Comparator.comparing(census -> (census.numberOf4sScored *4+census
-                .numberOf6sScored *6), Comparator.reverseOrder()));
-        this.sortByFields.put(SortingFields.MAX_4s_AND_6s_WITH_BEST_STRIKING_RATE, Comparator.comparing(census ->
-                ((census.numberOf4sScored *4+census.numberOf6sScored *6))/(census.ballsFaced), Comparator.reverseOrder()));
-        Comparator<IPLDAO> comparatorForBattingAverage = Comparator.comparing(compare -> compare.averageOfBatsmen);
-        this.sortByFields.put(SortingFields.BEST_AVERAGE_WITH_STRIKE_RATE, comparatorForBattingAverage.thenComparing(compare ->
-                compare.strikeRate).reversed());
-        Comparator<IPLDAO> comparatorForRuns = Comparator.comparing(compare -> compare.runsScored);
-        this.sortByFields.put(SortingFields.MAX_RUNS_WITH_BEST_AVERAGE, comparatorForRuns.thenComparing(compare ->
-                compare.averageOfBatsmen).reversed());
-        this.sortByFields.put(SortingFields.BOWLING_AVERAGE, Comparator.comparing(census -> census.averageOfBowler));
-        this.sortByFields.put(SortingFields.TOP_BOWLING_STRIKING_RATES, Comparator.comparing(census ->
-                census.strikeRatesOfBowler));
-        this.sortByFields.put(SortingFields.TOP_BOWLING_ECONOMY_RATES, Comparator.comparing(census ->
-                census.economyOfBowler));
-        Comparator<IPLDAO> comparatorForBowler = Comparator.comparing(census -> census.bowlersWith4Wickets
-                +census.bowlersWith5Wickets);
-        this.sortByFields.put(SortingFields.TOP_BOWLING_STRIKING_RATES_WITH_MOST_4Ws_AND_5Ws,
-                comparatorForBowler.thenComparing(compare -> compare.strikeRatesOfBowler).reversed());
-        Comparator<IPLDAO> comparatorForBestAverageBowler = Comparator.comparing(compare -> compare.averageOfBowler);
-        this.sortByFields.put(SortingFields.GREAT_AVERAGE_WITH_BEST_STRIKING_RATE,
-                comparatorForBestAverageBowler.thenComparing(compare -> compare.strikeRatesOfBowler));
-        this.sortByFields.put(SortingFields.BEST_BATTING_BOWLING_AVERAGE,
-                new BestBattingAndBowlingComparator().reversed());
-        this.sortByFields.put(SortingFields.ALL_ROUNDER,
-                new AllRounderComparator().reversed());
-    }
-
     public int loadIPLData(Innings innings,String... csvFilePath) throws IPLAnalyserException {
         IPLAdapter iplAdapter = IPLLoaderProvider.getIPLObject(innings);
-        this.iplRunsMap = iplAdapter.loadIPLData(innings, csvFilePath);
-        return this.iplRunsMap.size();
+        this.iplMap = iplAdapter.loadIPLData(innings, csvFilePath);
+        return this.iplMap.size();
     }
 
     public String sortByFields(SortingFields parameter) {
-        ArrayList getDTO = this.iplRunsMap.values().stream()
-                .sorted(this.sortByFields.get(parameter))
+        Comparator<IPLDAO> iplComparator = sortingForIPL.setSortByFields(parameter);
+        ArrayList getDTO = this.iplMap.values().stream()
+                .sorted(iplComparator)
                 .collect(Collectors.toCollection(ArrayList::new));
         String sortIplToJson = new Gson().toJson(getDTO);
         return sortIplToJson;
---
+++ src/main/java/com/iplanaylser/SortingForIPL.java
@@ -0,0 +1,48 @@
+package com.iplanaylser;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SortingForIPL {
+    static Map<SortingFields, Comparator<IPLDAO>> iplMap = new HashMap<>();
+
+    public SortingForIPL() {
+    }
+
+    public Comparator<IPLDAO> setSortByFields(SortingFields sortByFields) {
+        this.iplMap.put(SortingFields.BATTING_AVERAGE, Comparator.comparing(census -> census.averageOfBatsmen,
+                Comparator.reverseOrder()));
+        this.iplMap.put(SortingFields.STRIKING_RATE, Comparator.comparing(census ->
+                census.strikeRate, Comparator.reverseOrder()));
+        this.iplMap.put(SortingFields.MAX_4s_AND_6s, Comparator.comparing(census -> (census.numberOf4sScored *4+census
+                .numberOf6sScored *6), Comparator.reverseOrder()));
+        this.iplMap.put(SortingFields.MAX_4s_AND_6s_WITH_BEST_STRIKING_RATE, Comparator.comparing(census ->
+                ((census.numberOf4sScored *4+census.numberOf6sScored *6))/(census.ballsFaced), Comparator.reverseOrder()));
+        Comparator<IPLDAO> comparatorForBattingAverage = Comparator.comparing(compare -> compare.averageOfBatsmen);
+        this.iplMap.put(SortingFields.BEST_AVERAGE_WITH_STRIKE_RATE, comparatorForBattingAverage.thenComparing(compare ->
+                compare.strikeRate).reversed());
+        Comparator<IPLDAO> comparatorForRuns = Comparator.comparing(compare -> compare.runsScored);
+        this.iplMap.put(SortingFields.MAX_RUNS_WITH_BEST_AVERAGE, comparatorForRuns.thenComparing(compare ->
+                compare.averageOfBatsmen).reversed());
+        this.iplMap.put(SortingFields.BOWLING_AVERAGE, Comparator.comparing(census -> census.averageOfBowler));
+        this.iplMap.put(SortingFields.TOP_BOWLING_STRIKING_RATES, Comparator.comparing(census ->
+                census.strikeRatesOfBowler));
+        this.iplMap.put(SortingFields.TOP_BOWLING_ECONOMY_RATES, Comparator.comparing(census ->
+                census.economyOfBowler));
+        Comparator<IPLDAO> comparatorForBowler = Comparator.comparing(census -> census.bowlersWith4Wickets
+                +census.bowlersWith5Wickets);
+        this.iplMap.put(SortingFields.TOP_BOWLING_STRIKING_RATES_WITH_MOST_4Ws_AND_5Ws,
+                comparatorForBowler.thenComparing(compare -> compare.strikeRatesOfBowler).reversed());
+        Comparator<IPLDAO> comparatorForBestAverageBowler = Comparator.comparing(compare -> compare.averageOfBowler);
+        this.iplMap.put(SortingFields.GREAT_AVERAGE_WITH_BEST_STRIKING_RATE,
+                comparatorForBestAverageBowler.thenComparing(compare -> compare.strikeRatesOfBowler));
+        this.iplMap.put(SortingFields.BEST_BATTING_BOWLING_AVERAGE,
+                new BestBattingAndBowlingComparator().reversed());
+        this.iplMap.put(SortingFields.ALL_ROUNDER,
+                new AllRounderComparator().reversed());
+        Comparator<IPLDAO> comparator = iplMap.get(sortByFields);
+        return comparator;
+    }
+
+}
